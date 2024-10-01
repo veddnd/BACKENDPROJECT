b349a1f07ae2ff8d40c6e76ae65216e592807b68
veddnd <vedikadnd@gmail.com>
2024-10-01 22:27:12 +0530

    redoing the back changes

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,9 @@ import {DB_NAME} from "../constants.js"
 
 const connectdb= async()=>{
     try{
-        const connection=await mongoose.connect("mongodb+srv://vedikadnd:vedikadnd@cluster0.m1nc5.mongodb.net/Twitch")
+        const connection=await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(` mongodb connectted!! DB HOST`)
-        //console.log(connection)
+        console.log(connection)
     }catch(error){
         console.log("connection error",error);
         process.exit(1);
